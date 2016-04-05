3f9a10e36b5cb6922933d703ac65356fcc0f8670
Tiffany <tintingchiang@gmail.com>
2016-04-05 01:21:15 -0700

    created Items controller

--- UCUtrade/app/controllers/items_controller.rb
+++ UCUtrade/app/controllers/items_controller.rb
@@ -1,2 +1,24 @@
 class ItemsController < ApplicationController
+	def index
+		@items = Item.all
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.create(user_params)
+    	if @item.save #If saving the user was successful
+    		#flash[:error] = nil
+      		redirect_to :controller => '/item', :action => 'show'#show #Go to the show view of the user
+    	else
+    		#flash[:error] = @pokemon.errors.full_messages.to_sentence
+      		render "new" #Go to the new view for
+    	end
+	end
 end
