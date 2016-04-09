6bc2bbe50a0799019076633b44b8d05656835a32
Tiffany <tintingchiang@gmail.com>
2016-04-08 17:16:50 -0700

    killing my extra files

--- UCUtrade/app/controllers/items_controller.rb
+++ UCUtrade/app/controllers/items_controller.rb
@@ -13,6 +13,8 @@ class ItemsController < ApplicationController
 
 	def create
 		@item = Item.create(user_params)
+		@item.user_id = current_user.id
+		@item.available = true
     	if @item.save #If saving the user was successful
     		#flash[:error] = nil
       		redirect_to :controller => '/item', :action => 'show'#show #Go to the show view of the user
@@ -21,4 +23,27 @@ class ItemsController < ApplicationController
       		render "new" #Go to the new view for
     	end
 	end
+
+	def edit
+		@item = Item.find(params[:id])
+	end
+
+	def update
+		@item = Item.find(params[:id])
+    	if @item.update_attributes(item_params_edit)
+    		redirect_to => 'item', :action => 'show'
+    		# Handle a successful update.
+    	else
+    		render 'edit'
+    	end
+	end
+
+	private
+	def user_params
+  		params.require(:item).permit( :name, :price, :description)
+  	end
+
+  	def item_params_edit
+  		params.require(:item).permit( :name, :price, :description, :available)
+  	end
 end
