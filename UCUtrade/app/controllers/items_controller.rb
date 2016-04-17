a0bb97ce0e9e9cb47081db1f42d83c2f61c9b883
Tiffany <tingtingchiang@gmail.com>
2016-04-17 03:03:52 +0000

    replaced creating new items

--- UCUtrade/app/controllers/items_controller.rb
+++ UCUtrade/app/controllers/items_controller.rb
@@ -6,5 +6,26 @@ class ItemsController < ApplicationController
             @items = Item.search(params[:search])
         end
     end
+    
+	def new
+		@pitem = Item.new #Creates an empty user object.
+	end
+
+  	def create
+    	@item = Item.create(user_params)
+    	@item.available = true;
+    	@item.current_user = current_user.id
+    	if @item.save #If saving the user was successful
+    		flash[:error] = nil
+      		redirect_to current_user #Go to the show view of the user
+    	else
+    		flash[:error] = @item.errors.full_messages.to_sentence
+      		render "new" #Go to the new view for
+    	end
+  	end
+  	
+  	def edit
+  	end
+  	
 
 end
