a557904d24dc2aea41ec2c09446ca6cd4fff2612
Yitian Zou <yitian.zou@berkeley.edu>
2016-04-24 16:54:23 -0700

    minor search change

--- UCUtrade/app/models/item.rb
+++ UCUtrade/app/models/item.rb
@@ -6,10 +6,6 @@ class Item < ActiveRecord::Base
 
     def self.search(search)
         where("name LIKE ?", "%#{search}%")
-        # if search
-        #     find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-        # else
-        #     find(:all)
-        # end
+
     end
 end
