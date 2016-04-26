1d46903588900beaf915aecb09e86c8eedd13946
Igor Martire <igormartire@berkeley.edu>
2016-04-26 06:41:45 -0700

    adding new features to models and item box

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
     belongs_to :user
     has_many :comments, as: :commentable
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
     validates_attachment_content_type :avatar, content_type: ["image/jpeg", "image/gif", "image/png"]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :items
+  has_many :items, dependent: :delete
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
+
+  def username
+    self.email[/[^@]+/]
+  end
 end
