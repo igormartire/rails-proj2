5fb41463624f2f1babb0a6fc4a8e30d95885ce6f
Igor Martire <igormartire@berkeley.edu>
2016-04-26 03:48:56 -0700

    implementing aws s3

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,10 @@ class Item < ActiveRecord::Base
     belongs_to :user
     has_many :comments, as: :commentable
     has_many :comments
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+    has_attached_file :avatar,
+      styles: { medium: "300x300>", thumb: "100x100>" },:storage => :s3,
+      :path => ":attachment/:id/:style.:extension",
+      :bucket => "rorproj"
     validates_attachment_content_type :avatar, content_type: ["image/jpeg", "image/gif", "image/png"]
 
     def self.search(search)
