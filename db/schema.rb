1f0091388dbc2989be6f48cf41ee6e414f5c33ea
Igor Martire <igormartire@berkeley.edu>
2016-04-26 11:41:06 -0700

    remove image from item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426172647) do
+ActiveRecord::Schema.define(version: 20160426183930) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20160426172647) do
     t.string   "name"
     t.string   "description"
     t.integer  "price"
-    t.string   "image"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.integer  "user_id"
