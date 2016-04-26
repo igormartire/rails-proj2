87e7cd31c97689d783ff9dba03725c2bab5eb029
David Nahm <David@airbears2-10-142-70-235.airbears2.1918.berkeley.edu>
2016-04-26 00:18:53 -0700

    fixing sendgrid stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address    => "smtp.sendgrid.net",
     :port       => 25,
-    :user_name  => ENV['davidnahm'],
-    :password   => ENV['railsdecal123'],
-    :domain     => ENV['cryptic-beyond-59326.herokuapp.com'],
+    :user_name  => ENV['SENDGRID_USERNAME'],
+    :password   => ENV['SENDGRID_PASSWORD'],
+    :domain     => ENV['SENDGRID_DOMAIN'],
     :authentication  => :plain
   }
 end
