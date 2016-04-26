338c773b0f03f040e71876d3382794cb6be52354
Igor Martire <igormartire@berkeley.edu>
2016-04-26 03:56:35 -0700

    implementing aws s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
     :s3_protocol => :https,
     :path => ":attachment/:id/:style.:extension",
     :bucket => ENV['S3_BUCKET_NAME'],
-    :s3_host_name => 'https://rorproj.s3-us-west-1.amazonaws.com',
+    :s3_host_name => 'rorproj.s3-us-west-1.amazonaws.com',
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
