005129c470019f87dc6bf841bd9f8ee35d00ba00
David Nahm <David@airbears2-10-142-70-235.airbears2.1918.berkeley.edu>
2016-04-26 00:00:01 -0700

    working on sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'cryptic-beyond-59326.heroku.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address    => "smtp.sendgrid.net",
+    :port       => 25,
+    :user_name  => ENV['davidnahm'],
+    :password   => ENV['railsdecal123'],
+    :domain     => ENV['cryptic-beyond-59326.herokuapp.com'],
+    :authentication  => :plain
+  }
 end
