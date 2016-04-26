b171127a80609bcad06d4fd0d3b1f58247bdcb25
Igor Martire <igormartire@berkeley.edu>
2016-04-26 03:15:04 -0700

    implementing aws s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,10 @@ Rails.application.configure do
     :domain     => ENV['SENDGRID_DOMAIN'],
     :authentication  => :plain
   }
-  
+
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_region => ENV['AWS_REGION'],
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
